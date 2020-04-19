27fab6a361fd3ee75265937f35d444139e1e12fd
Keno <37253540+kbressem@users.noreply.github.com>
2020-04-19 23:04:46 +0200

    Add files via upload

---
+++ image-labeler.py
@@ -0,0 +1,283 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# https://gist.github.com/acbetter/32c575803ec361c3e82064e60db4e3e0#file-qimageviewer-py
+
+from PyQt5.QtCore import Qt, QRect, QMetaObject, QCoreApplication
+from PyQt5.QtGui import QImage, QPixmap, QPalette, QPainter
+from PyQt5.QtPrintSupport import QPrintDialog, QPrinter
+from PyQt5.QtWidgets import (QLabel, QSizePolicy, QScrollArea, QMessageBox, QMainWindow, QMenu, QAction, qApp, QFileDialog, QComboBox, QWidget)
+import os
+
+# for DICOM support
+from pydicom import dcmread
+from numpy import amax, uint8
+from PIL import Image
+
+import PIL
+PIL.__version__
+
+class QImageViewer(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.printer = QPrinter()
+        self.scaleFactor = 0.0
+
+        self.imageLabel = QLabel()
+        self.imageLabel.setBackgroundRole(QPalette.Base)
+        self.imageLabel.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.imageLabel.setScaledContents(True)
+
+        self.scrollArea = QScrollArea()
+        self.scrollArea.setBackgroundRole(QPalette.Dark)
+        self.scrollArea.setWidget(self.imageLabel)
+        self.scrollArea.setVisible(False)
+
+        self.setCentralWidget(self.scrollArea)
+
+        self.createActions()
+        self.createMenus()
+
+        self.setWindowTitle("Image Viewer")
+        self.resize(1800, 1400)
+        self.setupAnnotationArea()
+
+    def setupAnnotationArea(self):
+
+        self.loadPredefinedFindings()
+        self.numberFindings = len(self.findings)
+
+        # TODO: replace eval and exec with a dictionary
+        for i in range(0, self.numberFindings):
+            expr_name = 'self.finding_'+str(i)
+            expr_construct = '=QComboBox(self)'
+            expr_setGeometry = '.setGeometry(QRect(1410,'+str(i*30+10)+', 350, 25))'
+            expr_setObjectName = '.setObjectName("finding_'+str(i)+'")'
+            exec(expr_name+expr_construct)
+            eval(expr_name+expr_setGeometry)
+            eval(expr_name+expr_setObjectName)
+
+        self.setupComboBoxes()
+
+    def loadPredefinedFindings(self):
+        self.findings = []
+        with open("labels.txt", "r") as f:
+            for l in f.readlines():
+                self.findings.append(l.replace("\n", "").split(","))
+
+    def setupComboBoxes(self):
+        for i in range(0, self.numberFindings):
+            for f in self.findings[i]:
+                expr = 'self.finding_'+str(i)+'.addItem(f)'
+                eval(expr)
+
+    def writeFindings(self):
+        fileName = self.fileNames[self.imageNumber]
+        fileName = os.path.splitext(fileName)[0]
+        with open(fileName+"_annotation.txt", "w+") as a:
+            for i in range(0, self.numberFindings):
+                expr_name = 'self.finding_'+str(i)
+                a.write(eval(expr_name+'.currentText()')+'\n')
+
+    def loadFindings(self):
+        fileName = self.fileNames[self.imageNumber]
+        fileName = os.path.splitext(fileName)[0]+"_annotation.txt"
+        if os.path.isfile(fileName):
+            annotatedFinding=[]
+            with open(fileName, "r") as a:
+                for l in a.readlines():
+                    annotatedFinding.append(l.replace("\n", ""))
+
+            for i in range(0, self.numberFindings):
+                expr_name = 'self.finding_'+str(i)
+                eval(expr_name+'.setCurrentText(annotatedFinding['+str(i)+'])')
+
+    def open(self):
+        fileDir = QFileDialog.getExistingDirectory(self)
+        self.fileNames = []
+        self.imageExtensions = tuple([".png", ".jpeg", ".jpg", ".bmp", ".gif"])
+        for dirpath, subdirs, files in os.walk(fileDir):
+            for f in files:
+                if f.endswith(self.imageExtensions) or f.endswith(".dcm"):
+                    self.fileNames.append(os.path.join(dirpath, f))
+        self.imageNumber = 0
+        self.show_image(self.fileNames[self.imageNumber])
+        self.nextAct.setEnabled(not self.fitToWindowAct.isChecked())
+        self.previousAct.setEnabled(not self.fitToWindowAct.isChecked())
+
+    def show_image(self, fileName):
+        if fileName:
+            if fileName.endswith(self.imageExtensions):
+                image = QImage(fileName)
+                if image.isNull():
+                    QMessageBox.information(self, "Image Viewer", "Cannot load %s." % fileName)
+                    return
+
+                self.imageLabel.setPixmap(QPixmap.fromImage(image))
+            else:
+                # very rudimentary DICOM support
+                # TODO: adujsting grey level values with presets F1-F9 (simpler) or with mouse (probably help needed)
+
+                dicomFile = dcmread(fileName)
+                rescaleFactor = amax(dicomFile.pixel_array)/256
+                img = Image.fromarray((dicomFile.pixel_array/rescaleFactor).astype(uint8))
+                img.save(".tmp.png")
+                self.imageLabel.setPixmap(QPixmap(".tmp.png"))
+
+            self.scaleFactor = 1.0
+
+            self.scrollArea.setVisible(True)
+            self.printAct.setEnabled(True)
+            self.fitToWindowAct.setEnabled(True)
+            self.updateActions()
+
+            if not self.fitToWindowAct.isChecked():
+                self.imageLabel.adjustSize()
+
+            # rescale very large images
+            if self.imageLabel.pixmap().height() > 1400:
+                scaleFactor = 1400/self.imageLabel.pixmap().height()
+                self.imageLabel.resize(scaleFactor * self.imageLabel.pixmap().size())
+            elif self.imageLabel.pixmap().width() > 1400:
+                scaleFactor = 1400/self.imageLabel.pixmap().width()
+                self.imageLabel.resize(scaleFactor * self.imageLabel.pixmap().size())
+            # rescale very small images (CT, MRI), usually squared images. If not, then width
+            # will likely be the smallest
+            elif self.imageLabel.pixmap().width() < 512:
+                scaleFactor = 512/self.imageLabel.pixmap().width()
+                self.imageLabel.resize(scaleFactor * self.imageLabel.pixmap().size())
+
+    def print_(self):
+        dialog = QPrintDialog(self.printer, self)
+        if dialog.exec_():
+            painter = QPainter(self.printer)
+            rect = painter.viewport()
+            size = self.imageLabel.pixmap().size()
+            size.scale(rect.size(), Qt.KeepAspectRatio)
+            painter.setViewport(rect.x(), rect.y(), size.width(), size.height())
+            painter.setWindow(self.imageLabel.pixmap().rect())
+            painter.drawPixmap(0, 0, self.imageLabel.pixmap())
+
+    def next(self):
+        self.writeFindings()
+        if self.imageNumber < (len(self.fileNames)-1):
+            self.imageNumber += 1
+        else:
+            self.imageNumber = 0
+        self.loadFindings()
+        self.show_image(self.fileNames[self.imageNumber])
+
+
+    def previous(self):
+        self.writeFindings()
+        if self.imageNumber > 0:
+            self.imageNumber -= 1
+        else:
+            self.imageNumber = (len(self.fileNames)-1)
+        self.loadFindings()
+        self.show_image(self.fileNames[self.imageNumber])
+
+    def zoomIn(self):
+        self.scaleImage(1.25)
+
+    def zoomOut(self):
+        self.scaleImage(0.8)
+
+    def normalSize(self):
+        self.imageLabel.adjustSize()
+        self.scaleFactor = 1.0
+
+    def fitToWindow(self):
+        fitToWindow = self.fitToWindowAct.isChecked()
+        self.scrollArea.setWidgetResizable(fitToWindow)
+        if not fitToWindow:
+            self.normalSize()
+
+        self.updateActions()
+
+    def about(self):
+        QMessageBox.about(self, "About Image Viewer",
+                          "<p>The <b>Image Viewer</b> example shows how to combine "
+                          "QLabel and QScrollArea to display an image. QLabel is "
+                          "typically used for displaying text, but it can also display "
+                          "an image. QScrollArea provides a scrolling view around "
+                          "another widget. If the child widget exceeds the size of the "
+                          "frame, QScrollArea automatically provides scroll bars.</p>"
+                          "<p>The example demonstrates how QLabel's ability to scale "
+                          "its contents (QLabel.scaledContents), and QScrollArea's "
+                          "ability to automatically resize its contents "
+                          "(QScrollArea.widgetResizable), can be used to implement "
+                          "zooming and scaling features.</p>"
+                          "<p>In addition the example shows how to use QPainter to "
+                          "print an image.</p>")
+
+    def createActions(self):
+        self.openAct = QAction("&Open...", self, shortcut="Ctrl+O", triggered=self.open)
+        self.printAct = QAction("&Print...", self, shortcut="Ctrl+P", enabled=False, triggered=self.print_)
+        self.exitAct = QAction("E&xit", self, shortcut="Ctrl+Q", triggered=self.close)
+        self.zoomInAct = QAction("Zoom &In (25%)", self, shortcut="Ctrl++", enabled=False, triggered=self.zoomIn)
+        self.zoomOutAct = QAction("Zoom &Out (25%)", self, shortcut="Ctrl+-", enabled=False, triggered=self.zoomOut)
+        self.normalSizeAct = QAction("&Normal Size", self, shortcut="Ctrl+S", enabled=False, triggered=self.normalSize)
+        self.nextAct = QAction("Ne&xt image", self, shortcut=Qt.Key_Right, enabled=False, triggered=self.next)
+        self.previousAct = QAction("Previous Image", self, shortcut=Qt.Key_Left, enabled=False, triggered=self.previous)
+        self.fitToWindowAct = QAction("&Fit to Window", self, enabled=False, checkable=True, shortcut="Ctrl+F",
+                                      triggered=self.fitToWindow)
+        self.aboutAct = QAction("&About", self, triggered=self.about)
+        self.aboutQtAct = QAction("About &Qt", self, triggered=qApp.aboutQt)
+
+    def createMenus(self):
+        self.fileMenu = QMenu("&File", self)
+        self.fileMenu.addAction(self.openAct)
+        self.fileMenu.addAction(self.printAct)
+        self.fileMenu.addSeparator()
+        self.fileMenu.addAction(self.exitAct)
+
+        self.viewMenu = QMenu("&View", self)
+        self.viewMenu.addAction(self.zoomInAct)
+        self.viewMenu.addAction(self.zoomOutAct)
+        self.viewMenu.addAction(self.normalSizeAct)
+        self.viewMenu.addAction(self.nextAct)
+        self.viewMenu.addAction(self.previousAct)
+        self.viewMenu.addSeparator()
+        self.viewMenu.addAction(self.fitToWindowAct)
+
+        self.helpMenu = QMenu("&Help", self)
+        self.helpMenu.addAction(self.aboutAct)
+        self.helpMenu.addAction(self.aboutQtAct)
+
+        self.menuBar().addMenu(self.fileMenu)
+        self.menuBar().addMenu(self.viewMenu)
+        self.menuBar().addMenu(self.helpMenu)
+
+    def updateActions(self):
+        self.zoomInAct.setEnabled(not self.fitToWindowAct.isChecked())
+        self.zoomOutAct.setEnabled(not self.fitToWindowAct.isChecked())
+        self.normalSizeAct.setEnabled(not self.fitToWindowAct.isChecked())
+
+    def scaleImage(self, factor):
+        self.scaleFactor *= factor
+        self.imageLabel.resize(self.scaleFactor * self.imageLabel.pixmap().size())
+
+        self.adjustScrollBar(self.scrollArea.horizontalScrollBar(), factor)
+        self.adjustScrollBar(self.scrollArea.verticalScrollBar(), factor)
+
+        self.zoomInAct.setEnabled(self.scaleFactor < 3.0)
+        self.zoomOutAct.setEnabled(self.scaleFactor > 0.333)
+
+    def adjustScrollBar(self, scrollBar, factor):
+        scrollBar.setValue(int(factor * scrollBar.value()
+                               + ((factor - 1) * scrollBar.pageStep() / 2)))
+
+    def closeEvent(self, event):
+        os.remove(".tmp.png")
+
+if __name__ == '__main__':
+    import sys
+    from PyQt5.QtWidgets import QApplication
+
+    app = QApplication(sys.argv)
+    imageViewer = QImageViewer()
+
+    imageViewer.show()
+
+    sys.exit(app.exec_())
